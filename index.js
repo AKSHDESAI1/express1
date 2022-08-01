dd1f96afe771227117410b9bbe9628c7c030669f
AKSHDESAI1 <desaiaksh82@gmail.com>
2022-08-01 19:46:48 +0530

    tenth

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './routes/web.js';
+import { join } from 'path';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,7 +11,7 @@ app.set('view engine', 'ejs')
 app.use('/', router)
 
 // Connect Static Files 
-app.use(express.static('public'));
+app.use(express.static(join(process.cwd(), 'public')));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
